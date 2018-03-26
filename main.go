7e10b6cdfa050be22cfe2b02a8ffa3f73c771681
Cao Nguyen V <cao.nguyen@asiantech.vn>
2018-03-26 11:23:10 +0700

    create route

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"html/template"
+	"go2-t2/router"
 	"log"
 	"net/http"
 )
@@ -10,24 +10,8 @@ import (
 func main() {
 	fmt.Print("HELLO GOLANG")
 	log.Println("Server started on: http://localhost:8080")
-	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
-	http.HandleFunc("/", index)
-	http.HandleFunc("/edit", edit)
-	http.HandleFunc("/detail", detail)
-	http.ListenAndServe(":8080", nil)
-}
 
-func index(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("views/layout/header.html", "views/blogs/index.html", "views/layout/footer.html"))
-	tmpl.ExecuteTemplate(w, "index", nil)
-}
+	r := router.Route()
 
-func edit(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("views/layout/header.html", "views/blogs/edit.html", "views/layout/footer.html"))
-	tmpl.ExecuteTemplate(w, "edit", nil)
-}
-
-func detail(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("views/layout/header.html", "views/blogs/detail.html", "views/layout/footer.html"))
-	tmpl.ExecuteTemplate(w, "detail", nil)
+	http.ListenAndServe(":8080", r)
 }
