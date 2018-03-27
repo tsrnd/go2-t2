2a39e82868f65cfa5ce1c9995a500f43efe1f59c
Cao Nguyen V <cao.nguyen@asiantech.vn>
2018-03-27 10:15:02 +0700

    edit package mux to chichi, fix bloghangler folder

---
+++ handler/BlogHandler.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"html/template"
+	"net/http"
+)
+
+type BlogHandler struct{}
+
+func getTemplate(str string) *template.Template {
+	str = "views/blogs/" + str
+	tmpl := template.Must(template.ParseFiles("views/layout/header.html", str, "views/layout/footer.html"))
+	return tmpl
+}
+
+func (bh BlogHandler) Index(w http.ResponseWriter, r *http.Request) {
+	tmpl := getTemplate("index.html")
+	tmpl.ExecuteTemplate(w, "index", nil)
+}
+
+func (bh BlogHandler) Edit(w http.ResponseWriter, r *http.Request) {
+	tmpl := getTemplate("edit.html")
+	tmpl.ExecuteTemplate(w, "edit", nil)
+}
+
+func (bh BlogHandler) Detail(w http.ResponseWriter, r *http.Request) {
+	tmpl := getTemplate("detail.html")
+	tmpl.ExecuteTemplate(w, "detail", nil)
+}
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"go2-t2/config"
 	"go2-t2/model"
 	"go2-t2/router"
@@ -13,12 +12,9 @@ import (
 )
 
 func main() {
-	fmt.Print("HELLO GOLANG")
-
 	log.Printf("Server started on: http://localhost%s", os.Getenv("SERVER_PORT"))
 
 	r := router.Route()
-
 	http.ListenAndServe(os.Getenv("SERVER_PORT"), r)
 }
 
