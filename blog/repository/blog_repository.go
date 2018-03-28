499a200a25e160f674953c98aeab94f190bf1def
Nghia Vo V <nghia.vo@asiantech.vn>
2018-03-28 17:55:22 +0700

    change query select all posts

--- blog/repository/blog_repository.go
+++ blog/repository/blog_repository.go
@@ -2,11 +2,15 @@ package repository
 
 import (
 	model "go2-t2/model"
+	"log"
 )
 
 // GetAllPosts return all posts
 func GetAllPosts() []model.Blog {
 	var blogs []model.Blog
-	model.DBCon.Find(&blogs)
+	allPosts := model.DBCon.Select("id, title, content").Order("id desc, title").Find(&blogs)
+	if allPosts.Error != nil {
+		log.Fatalln(allPosts.Error)
+	}
 	return blogs
 }
