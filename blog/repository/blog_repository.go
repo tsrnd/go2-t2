eb1cb84e1488d3c7bce3abbd2fa6da4de6a5abd6
Nghia Vo V <nghia.vo@asiantech.vn>
2018-03-28 14:30:03 +0700

    remove interface not use

--- blog/repository/blog_repository.go
+++ blog/repository/blog_repository.go
@@ -2,16 +2,7 @@ package repository
 
 import (
 	model "go2-t2/model"
-
-	"github.com/jinzhu/gorm"
 )
-
-var db *gorm.DB
-
-// BlogRepository interface
-type BlogRepository interface {
-	GetAllPosts() (*model.Blog, error)
-}
 
 // GetAllPosts return all posts
 func GetAllPosts() []model.Blog {
