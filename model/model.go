860233c18e0fed30a1709a03ca4600ec46548ffc
Nghia Vo V <nghia.vo@asiantech.vn>
2018-03-28 09:15:16 +0700

    show database to index page

--- model/model.go
+++ model/model.go
@@ -1,11 +1,27 @@
 package model
 
-import "database/sql"
+import (
+	"github.com/jinzhu/gorm"
+)
 
 //DBCon dbcon
-var DBCon *sql.DB
+var DBCon *gorm.DB
+
+// Blog is type of blog
+type Blog struct {
+	ID      int
+	Title   string
+	Content string
+}
 
 //SetDatabase return DBCon
-func SetDatabase(database *sql.DB) {
+func SetDatabase(database *gorm.DB) {
 	DBCon = database
+}
+
+// GetAllPosts return all posts
+func GetAllPosts() []Blog {
+	var blogs []Blog
+	DBCon.Select("id, title, content").Order("id desc, title").Find(&blogs)
+	return blogs
 }
